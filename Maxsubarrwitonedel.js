1be5713ce2bce89e26e3d7ef2d3a5248cba04bd8
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-10-17 12:20:09 -0600

    Update Maxsubarrwitonedel.js

--- Maxsubarrwitonedel.js
+++ Maxsubarrwitonedel.js
@@ -35,7 +35,7 @@ Memory Usage: 43.1 MB, less than 97.44% of JavaScript online submissions for Max
 
 
 //2nd solution: Kadane's Algorithm:
-
+//TC: O(nlogn)
 const maximumSum = (arr) => {
   if (arr.length == 1) {
 			return arr[0];
