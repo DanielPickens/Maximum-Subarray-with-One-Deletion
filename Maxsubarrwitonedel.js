93e23ec150ad880a1c8177b6a3ad827e38c9354f
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-10-16 21:13:42 -0600

    Update Maxsubarrwitonedel.js

--- Maxsubarrwitonedel.js
+++ Maxsubarrwitonedel.js
@@ -30,3 +30,40 @@ Details
 Runtime: 86 ms, faster than 82.05% of JavaScript online submissions for Maximum Subarray Sum with One Deletion.
 Memory Usage: 43.1 MB, less than 97.44% of JavaScript online submissions for Maximum Subarray Sum with One Deletion.
 */
+
+
+
+
+
+
+//2nd solution: Using Kadanes Algorithm:
+
+const maximumSum = function(arr) {
+  if (arr.length == 1) {
+			return arr[0];
+		}
+		
+		let ans = Number.MIN_VALUE;
+		let lkad = [arr.length];
+		lkad[0]=arr[0];
+		ans=arr[0];
+		
+		//finding left kadane for each index
+		for (let i = 1; i < arr.length; i++) {
+			lkad[i]=Math.max(lkad[i-1]+arr[i],arr[i]);
+			ans=Math.max(ans,lkad[i]);
+		}
+		
+		ans = Math.max(ans, lkad[arr.length - 2]);//if last element is deleted
+		
+		//finding right kadane for each index and checking if deletion updates answer
+		let rkad = arr[arr.length - 1];
+		for (let i = arr.length - 2; i > 0; i--) {
+			ans = Math.max(ans, lkad[i - 1] + rkad);
+			rkad = Math.max(arr[i], rkad + arr[i]);
+		}
+		
+		ans = Math.max(ans, rkad);//if first element is deleted
+		
+		return ans;
+	}
