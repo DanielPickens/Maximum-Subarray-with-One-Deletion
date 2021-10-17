e9b64d32c92310e39b53ee0c80342c6d68ff5f0b
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-10-16 21:08:05 -0600

    Update Maxsubarrwitonedel.js

--- Maxsubarrwitonedel.js
+++ Maxsubarrwitonedel.js
@@ -1,12 +1,17 @@
 //Dynamic Programming 
-//Time Complexity: O(1)
+//Time Complexity: O(n)  
+
+/*
+dp[i][0] represents the maximum sum at position i with 0 deletions
+           dp[i][1] represents the maximum sum at position i with 1 deletions 
+*/
 var maximumSum = (arr) => {
     let maxWithNoDeletions = arr[0],
             maxWithOneDeletion = arr[0],
             maxOverall = arr[0];
-        for (let i = 1; i < arr.length; i++) {
-            let numToProcess = arr[i],
-                nextMaxWithNoDeletions =
+        for (let i = 1; i < arr.length; i++) {    //Essentially, if there are no deletions that occur, then the max sum at postion i is equal to max of the maximum sum at positon i - 1 or starting a new sum at position i
+            let numToProcess = arr[i],     //Notice that dp is subscripted from 1 to n, hence we take arr[i-1] in the for loop
+                nextMaxWithNoDeletions =      //If there is 1 deletion we have 2 choices either delete the element at position i or not delete the element at position i. If we delete the element at position i , dp[i][1] = dp[i-1][0] . If we don't delete the element at position i we add the value of the element at position i 
                     Math.max(maxWithNoDeletions + numToProcess, numToProcess),
                 nextMaxWithOneDeletion =
                     Math.max(maxWithOneDeletion + numToProcess, maxWithNoDeletions);
